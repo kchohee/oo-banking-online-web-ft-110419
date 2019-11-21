d58d7ca17104104fa7888cd2b5799fd2559a5ffc
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 17:54:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,4 @@
 class Transfer
-<<<<<<< HEAD
   attr_accessor :amount, :status, :sender, :receiver
   def initialize(sender, receiver,amount)
     self.sender = sender
@@ -29,12 +28,10 @@ class Transfer
       self.receiver.deposit(-1*self.amount)
       self.status = "reversed"
     end
-=======
   attr_accessor :amount, :status, :sender, :receiever
   def initialize(sender, receiever,amount)
     self.sender = sender
     self.receiever = receiever
     self.amount = amount
->>>>>>> 5d76f5d620dc5f9cf72af5fd27e3c43536795acb
   end
 end
